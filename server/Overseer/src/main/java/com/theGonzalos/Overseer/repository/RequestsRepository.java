10b148639b3447be28c38f397ebec87614b02c5a
Mehzabin Sadat Aothoi <mehzabin.sadat.aothoi@g.bracu.ac.bd>
2022-07-22 01:10:12 +0600

    Created Requests logic

---
+++ server/Overseer/src/main/java/com/theGonzalos/Overseer/repository/RequestsRepository.java
@@ -0,0 +1,7 @@
+package com.theGonzalos.Overseer.repository;
+
+import com.theGonzalos.Overseer.model.Requests;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RequestsRepository extends JpaRepository<Requests, Integer> {
+}
